712503ee2d11f1defc11a4b0f59850149188c18f
hfog2066 <hfog2000@yahoo.com>
2020-10-21 13:07:04 -0500

    sort.h prototypes.

--- sort.h
+++ sort.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stddef.h>
+#include <stdio.h>
 
 
 /**
@@ -19,18 +20,14 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* Print Functions */
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void print_list_rev(const listint_t *list);
-
-/* Mandatory tasks */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
-/* Advanced tasks */
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
