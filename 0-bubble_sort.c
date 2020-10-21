f8a6d8626c7b44f138aba22e1862c374dcb0e4da
hfog2066 <hfog2000@yahoo.com>
2020-10-21 18:33:12 -0500

    0. Bubble sort.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,13 +1,11 @@
 #include "sort.h"
 
-
 /**
- * bubble_sort - sorts an array of integers in ascending order using the
- * Bubble sort algorithm
- * @array: data to sort
- * @size: size of data
- * Return: No Return
- */
+ * bubble_sort - sorts an array of integers in ascending order using
+ * the Bubble sort algorithm
+ * @array: pointer to array
+ * @size: size of the array
+ **/
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, tmp;
