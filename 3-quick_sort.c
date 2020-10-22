8d08fcb36893227cb8745d5ec9ac66d49ec6eddd
hfog2066 <hfog2000@yahoo.com>
2020-10-21 19:05:46 -0500

    3. Quick sort.

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+
 /**
  * quick_sort_rec - sorts an array of integers in ascending order using
  * the Selection sort algorithm using recursion
@@ -6,7 +7,7 @@
  * @size: size of the array
  * @array_init: initial pointer to array
  * @size_init: initial size of the array
- **/
+ */
 void quick_sort_rec(int *array_init, size_t size_init, int *array, size_t size)
 {
 	size_t j, aux;
